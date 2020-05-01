5724134b60904dda1e631b709fb38ca14ecbb05e
Rohit Jadhav <rohitjadhav@Rohits-MacBook-Air.local>
2020-05-01 17:05:39 +0530

    add the form code on login view page

--- application/views/users/login_view.php
+++ application/views/users/login_view.php
@@ -1,1 +1,7 @@
 <h2>Login Form</h2>
+
+<?php $attributes = ['id'=>'login_form','class'=>'form_horizontal']; ?>
+
+<?php echo form_open('users/login_view',$attributes); ?>
+
+<?php echo form_close(); ?>
